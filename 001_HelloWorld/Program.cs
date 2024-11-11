2b421a9fe13ea543ffafdba940d51af75f810a33
PPUDANI <zxvk1030@naver.com>
2024-11-11 15:42:32 +0900

    업데이트

---
+++ 001_HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static System.Console;
+
+namespace _001_HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : HelloWorld.exe <이름>");
+                return;
+            }
+
+            Console.WriteLine("Hello, {0}!", args[1]);
+        }
+    }
+}
--- 00_using/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-// using -> include와 비슷함.
-// System이라는 구역 안의 코드를 사용하겠다는 뜻.
